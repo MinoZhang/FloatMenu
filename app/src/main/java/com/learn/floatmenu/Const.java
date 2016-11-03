5630ca7e4a400532086d28228c6dedde096f1636
MinoZhang <zhangboya@tarenwang.com>
2016-11-03 17:18:00 +0800

    有点问题需要处理

---
+++ app/src/main/java/com/learn/floatmenu/Const.java
@@ -0,0 +1,17 @@
+package com.learn.floatmenu;
+
+/**
+ * @description: Menu菜单常量
+ * @author MinoZhang
+ */
+
+public interface Const {
+    String GAME_URL = "http://xs.qidian.com/html/index.shtml";
+    String HOME = "首页";
+    String FAVORITE = "收藏";
+    String CUSTOMER_SERVICE = "客服";
+    String MESSAGE = "消息";
+    String CLOSE = "关闭";
+
+    String[] MENU_ITEMS = {HOME,FAVORITE,CUSTOMER_SERVICE,MESSAGE,CLOSE};
+}
--- app/src/main/java/com/learn/floatmenu/MainActivity.java
+++ app/src/main/java/com/learn/floatmenu/MainActivity.java
@@ -1,13 +1,41 @@
 package com.learn.floatmenu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    @BindView(R.id.start)
+    TextView mStart;
+    @BindView(R.id.hideStatuBarNaviBar)
+    Button mHideStatuBarNaviBar;
+    private TextView startService;
+    private Button hideService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.start:
+                Toast.makeText(this, "开启服务", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.hideStatuBarNaviBar:
+                break;
+            default:
+                break;
+        }
     }
 }
