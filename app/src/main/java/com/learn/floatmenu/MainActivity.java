1ca38335164548ef298a3e0c0da0634498c56277
MinoZhang <zhangboya@tarenwang.com>
2016-11-04 17:03:19 +0800

    去除butterknife插件使用，有问题

--- app/src/main/java/com/learn/floatmenu/MainActivity.java
+++ app/src/main/java/com/learn/floatmenu/MainActivity.java
@@ -5,16 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-    @BindView(R.id.start)
-    TextView mStart;
-    @BindView(R.id.hideStatuBarNaviBar)
-    Button mHideStatuBarNaviBar;
     private TextView startService;
     private Button hideService;
 
@@ -22,20 +14,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ButterKnife.bind(this);
     }
 
 
     @Override
     public void onClick(View view) {
-        switch (view.getId()){
-            case R.id.start:
-                Toast.makeText(this, "开启服务", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.hideStatuBarNaviBar:
-                break;
-            default:
-                break;
-        }
+        
     }
 }
